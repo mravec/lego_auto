aa37b0846f00ed57b4a226bf78bab554c4df3dba
mravec <48887485+mravec@users.noreply.github.com>
2021-07-05 19:37:17 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     cuteBot.motors(0, 0)
 })
+function vypis () {
+    serial.writeLine("name=" + "" + " value=" + value + " time=" + input.runningTime())
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "lv") {
         lavyMotor = value
@@ -8,30 +11,34 @@ radio.onReceivedValue(function (name, value) {
     if (name == "pv") {
         pravyMotor = value
     }
-    basic.pause(100)
 })
 let prekazka = 0
 let pravyMotor = 0
 let lavyMotor = 0
+serial.redirectToUSB()
+serial.writeLine("Start")
 radio.setGroup(1)
 basic.showLeds(`
     . . . . .
     . # # # .
-    . # . # .
+    . # # # .
     . # # # .
     . . . . .
     `)
 lavyMotor = 0
 pravyMotor = 0
 basic.forever(function () {
-    prekazka = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
-    if (prekazka < 30 && (lavyMotor != 0 && pravyMotor != 0)) {
-        cuteBot.motors(-10, -10)
-        basic.pause(500)
-        cuteBot.motors(0, 0)
-        basic.pause(200)
-    } else {
-        cuteBot.motors(lavyMotor, pravyMotor)
+    if (false) {
+        prekazka = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+        if (prekazka < 30 && (lavyMotor != 0 && pravyMotor != 0)) {
+            cuteBot.motors(-10, -10)
+            basic.pause(500)
+            cuteBot.motors(0, 0)
+            basic.pause(200)
+        } else {
+        	
+        }
     }
+    cuteBot.motors(lavyMotor, pravyMotor)
     basic.pause(100)
 })
