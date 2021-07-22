8cd0528040aed201991ae03ef4550ab413895c88
mravec <48887485+mravec@users.noreply.github.com>
2021-07-22 19:32:15 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -53,27 +53,34 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
 })
 function nastav_servo_otacania (kompas_ovladac: number) {
     if (rychlost != 0) {
-        kompas_auto = input.compassHeading()
-        kompas_zmena = kompas_ovladac - kompas_auto
-        if (kompas_zmena > 180) {
-            kompas_zmena += -360
-        }
-        if (kompas_zmena < -180) {
-            kompas_zmena += 360
-        }
-        if (kompas_zmena < -45) {
-            pins.servoWritePin(AnalogPin.P1, 150)
-        } else if (kompas_zmena > 45) {
-            pins.servoWritePin(AnalogPin.P1, 85)
+        if (kalibrovat_kompas == 0) {
+            kalibrovat_kompas = 1
+            kompas_auto = input.compassHeading()
+            kompas_kalibracia = kompas_ovladac - kompas_auto
         } else {
-            pins.servoWritePin(AnalogPin.P1, Math.map(kompas_zmena, -45, 45, 150, 85))
+            kompas_auto = input.compassHeading()
+            kompas_zmena = kompas_ovladac - kompas_auto - kompas_kalibracia
+            if (kompas_zmena > 180) {
+                kompas_zmena += -360
+            }
+            if (kompas_zmena < -180) {
+                kompas_zmena += 360
+            }
+            if (kompas_zmena < -45) {
+                pins.servoWritePin(AnalogPin.P1, 150)
+            } else if (kompas_zmena > 45) {
+                pins.servoWritePin(AnalogPin.P1, 85)
+            } else {
+                pins.servoWritePin(AnalogPin.P1, Math.map(kompas_zmena, -45, 45, 150, 85))
+            }
         }
-        vypis()
     }
 }
+let kompas_kalibracia = 0
 let kompas_auto = 0
 let kompas_zmena = 0
 let aktualny_cas = 0
+let kalibrovat_kompas = 0
 let servo_otacania = 0
 let rychlost = 0
 let stary_cas = 0
@@ -90,3 +97,4 @@ stary_cas = 0
 rychlost = 0
 soundExpression.happy.play()
 servo_otacania = 90
+kalibrovat_kompas = 0
