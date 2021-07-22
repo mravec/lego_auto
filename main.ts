b420a854c0f150699fec34befa70436857a7bdf5
mravec <48887485+mravec@users.noreply.github.com>
2021-07-22 17:19:13 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 function nastav_servo_rychlosti (nova_rychlost: number) {
     rychlost = nova_rychlost
+    pins.servoWritePin(AnalogPin.P2, Math.map(rychlost, -100, 100, 0, 180))
 }
 input.onButtonPressed(Button.A, function () {
     nastav_servo_rychlosti(100)
@@ -16,7 +17,7 @@ function vypis () {
     aktualny_cas = input.runningTime()
     if (aktualny_cas - stary_cas > 1000) {
         stary_cas = aktualny_cas
-        serial.writeLine("rychlost" + rychlost + ", kompas=" + kompas)
+        serial.writeLine("rychlost" + rychlost + ", kompas=" + "")
     }
 }
 input.onButtonPressed(Button.B, function () {
@@ -35,13 +36,17 @@ radio.onReceivedValue(function (name, value) {
         nastav_servo_rychlosti(value)
     }
     if (name == "k") {
-        kompas = value
+        nastav_servo_otacania(value)
     }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     nastav_servo_rychlosti(0)
 })
-let kompas = 0
+function nastav_servo_otacania (kompas_ovladac: number) {
+    kompas_auto = input.compassHeading()
+    pins.servoWritePin(AnalogPin.P1, 0)
+}
+let kompas_auto = 0
 let stary_cas = 0
 let aktualny_cas = 0
 let rychlost = 0
@@ -57,6 +62,5 @@ basic.showLeds(`
 rychlost = 0
 soundExpression.happy.play()
 basic.forever(function () {
-    cuteBot.motors(0, -65)
     basic.pause(100)
 })
